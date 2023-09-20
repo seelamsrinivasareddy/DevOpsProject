486cb79a46b336f2f2b1608c959dcd77fa4211fb
Admin <Admin@DESKTOP-EH527SN>
2023-09-20 15:52:59 +0530

    Srinu Commit

---
+++ src/com/lbrce/DevOpsProject/CseBSection.java
@@ -0,0 +1,14 @@
+package com.lbrce.DevOpsProject;
+
+public class CseBSection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		srinu();
+	}
+	public static void srinu() {
+		// TODO Auto-generated method stub
+		System.out.println("This is Srinu");
+	}
+
+}
